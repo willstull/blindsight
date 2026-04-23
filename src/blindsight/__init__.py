c0aac02f91b8cbdd9a8d67dafddf660b408fa053
Will Stull <willstull17@gmail.com>
2026-04-23 10:52:27 -0400

    chore: prepare 0.0.1 reservation release

---
+++ src/blindsight/__init__.py
@@ -0,0 +1,7 @@
+"""Blindsight: agentic incident investigation framework.
+
+This is a placeholder release to reserve the package name on PyPI.
+The full package is under active development at
+https://github.com/willstull/blindsight.
+"""
+__version__ = "0.0.1"
